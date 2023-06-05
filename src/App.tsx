289df88acf5516978b6e8ced1558840c71ccc778
AliRadmanesh <ali.radmanesh1995@gmail.com>
2023-06-06 00:16:53 +0330

    feat: Add Marker with Popup on it

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 import 'leaflet/dist/leaflet.css'
 import './App.css'
 
+const IRAN_CENTER: [number, number] = [32.42, 53.68]
+
 function App() {
-  const IRAN_CENTER: [number, number] = [32.42, 53.68]
   return (
       <main>
         <MapContainer center={IRAN_CENTER} zoom={5}>
           <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' />
+          <Marker position={IRAN_CENTER}>
+            <Popup>
+              Hello, IRAN!
+            </Popup>
+          </Marker>
         </MapContainer>
       </main>
   )
