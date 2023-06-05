f8f68fcaf018099a9561bf59c1074ff93b5779de
AliRadmanesh <ali.radmanesh1995@gmail.com>
2023-06-06 00:02:55 +0330

    feat: Add MapContainer & OpenStreetMap TileLayer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { MapContainer, TileLayer } from 'react-leaflet'
+
+import 'leaflet/dist/leaflet.css'
 import './App.css'
 
 function App() {
+  const IRAN_CENTER: [number, number] = [32.42, 53.68]
   return (
-      <div>
-        New React Project
-      </div>
+      <main>
+        <MapContainer center={IRAN_CENTER} zoom={5}>
+          <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' />
+        </MapContainer>
+      </main>
   )
 }
 
